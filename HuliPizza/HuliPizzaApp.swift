b4d580d90edd3116ef7dea456569923b90596138
Rajesh Kumar <rajeshpremani68@gmail.com>
2023-03-24 17:10:30 +0500

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Rajesh Kumar Sahil on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
